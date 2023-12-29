bcf4125572a0ddb4c66c1845c103f14ac90dbc9a
Mohamedreda03 <143864316+Mohamedreda03@users.noreply.github.com>
2023-12-29 14:29:02 +0200

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/tests/12-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-sys.path.append('../')
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-
-fizzbuzz()
-print("")
--- 0x01-python-if_else_loops_functions/tests/8-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-sys.path.append('../')
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton School 98 Battery street")
